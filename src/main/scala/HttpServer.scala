08003183e1ca97dc3d4d2fe86647438cd1725f9b
jenoh <memint.contact@gmail.com>
2023-06-15 15:40:58 +0200

    remove comments

--- src/main/scala/HttpServer.scala
+++ src/main/scala/HttpServer.scala
@@ -10,19 +10,18 @@ import scala.io.StdIn
 
 object HttpServer {
 
-  // needed to run the route
   implicit val system: ActorSystem[Nothing] = ActorSystem(Behaviors.empty, "SprayExample")
-  // needed for the future map/flatmap in the end and future in fetchItem and saveOrder
   implicit val executionContext: ExecutionContextExecutor = system.executionContext
+
   def route: Route = path("tutorials") {    Directives.get {      complete("all tutorials")    }  }
 
   def main(args: Array[String]): Unit = {
 
     val bindingFuture = Http().newServerAt("localhost", 8080).bind(route)
     println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
-    StdIn.readLine() // let it run until user presses return
+    StdIn.readLine()
     bindingFuture
-      .flatMap(_.unbind()) // trigger unbinding from the port
-      .onComplete(_ => system.terminate()) // and shutdown when done
+      .flatMap(_.unbind())
+      .onComplete(_ => system.terminate())
   }
 }
