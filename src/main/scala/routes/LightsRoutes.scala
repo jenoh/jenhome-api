81e60165a6586e94d2bb207431870143b2967068
jenoh <memint.contact@gmail.com>
2023-07-13 11:48:10 +0200

    Add parameters to routing

--- src/main/scala/routes/LightsRoutes.scala
+++ src/main/scala/routes/LightsRoutes.scala
@@ -1,20 +1,26 @@
 package routes
 
-import akka.http.scaladsl.server.Directives.{complete, path, pathPrefix}
+import akka.http.scaladsl.server.Directives.{complete, parameters, path, pathPrefix}
 import akka.http.scaladsl.server.{Directives, Route}
 import services.LightsServices
+
+import javax.swing.text.Segment
 
 class LightsRoutes(lightsServices: LightsServices) {
   def route: Route = pathPrefix("lights") {
     Directives.concat(
       path("on") {
-        Directives.put {
-          complete(lightsServices.on)
+        parameters("id") { (id) =>
+          Directives.put {
+            complete(lightsServices.on(id))
+          }
         }
       },
       path("off") {
-        Directives.put {
-          complete(lightsServices.off)
+        parameters("id") { (id) =>
+          Directives.put {
+            complete(lightsServices.off(id))
+          }
         }
       })
   }
